3523d059daee2005fd68ea72bce934b45c5797a0
harijendra <informharijendra@gmail.com>
2026-07-18 15:43:37 +0530

    Add files via upload

---
+++ src/main/java/demo1/Demo.java
@@ -0,0 +1,14 @@
+package demo1;
+
+import org.apache.commons.lang.StringUtils;
+
+public class Demo {
+
+    public static void main(String[] args) {
+        String name = " ";
+
+        if (StringUtils.isBlank(name)) {
+            System.out.println("Blank");	
+        } 
+    }
+}   
